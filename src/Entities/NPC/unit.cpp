d9ce8d9bde8b97954afd95339cabe890fb1f22e9
Muneeb Haq <muh5329@gmail.com>
2025-05-15 23:54:35 -0400

    clean up

--- src/Entities/NPC/unit.cpp
+++ src/Entities/NPC/unit.cpp
@@ -35,27 +35,20 @@ class Unit {
     
         void Update(float deltaTime) {
             if (moving) {
-                Vector3 direction = { target.x - position.x, 0, target.z - position.z };
-                float distance = std::sqrt(direction.x * direction.x + direction.z * direction.z);
-                if (distance > 0.1f) {
-                    direction.x /= distance;
-                    direction.z /= distance;
-                    position.x += direction.x * Constants::UNIT_SPEED * deltaTime;
-                    position.z += direction.z * Constants::UNIT_SPEED * deltaTime;
-                    // Get position from physics body
-                    if (body) {
-                        btTransform trans;
-                        body->getMotionState()->getWorldTransform(trans);
-                        btVector3 pos = trans.getOrigin();
-                        position = { pos.getX(), pos.getY(), pos.getZ() };
-                    }
-                } else {
-                    moving = false;
+                // Get position from physics body
+                if (body) {
+                    btTransform trans;
+                    body->getMotionState()->getWorldTransform(trans);
+                    btVector3 pos = trans.getOrigin();
+                    position = { pos.getX(), pos.getY(), pos.getZ() };
                 }
+            } else {
+                moving = false;
             }
         }
+        }
 
-        void 
+         
     
         void Draw() const {
             Color color = selected ? RED : BLUE;
